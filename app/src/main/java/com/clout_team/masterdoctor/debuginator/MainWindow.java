05d9c79fdbe300b6fdb36d27b656520b4a479dcd
MasterDoctor <masterdoctor@goldblockstudios.com>
2017-01-28 19:07:03 +0000

    Initial Commit

---
+++ app/src/main/java/com/clout_team/masterdoctor/debuginator/MainWindow.java
@@ -0,0 +1,13 @@
+package com.clout_team.masterdoctor.debuginator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainWindow extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_window);
+    }
+}
